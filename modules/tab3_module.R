32e8351768c9b9c8fbaef5cf08b3f4b446a135aa
LNhart <lukastilmann97@gmail.com>
2024-03-05 19:16:15 +0100

    functionality to add new stratification layers and chose column for each layer

--- modules/tab3_module.R
+++ modules/tab3_module.R
@@ -9,6 +9,11 @@ tab3ui <- function(id){
     sidebarLayout(
       sidebarPanel(
         actionButton(ns("filter_dropdown"), "Filtern", icon = icon("filter")),
+        br(),
+        uiOutput(ns("strat_layer_buttons_ui")),
+        actionButton(ns("add_strat_layer_button"), "Schicht hinzufügen", 
+                     icon = icon("plus")),
+        uiOutput(ns("strata_rename_input_ui")),
         conditionalPanel(
           condition = "input.filter_dropdown % 2 != 0",
           ns = ns,
@@ -27,15 +32,21 @@ tab3ui <- function(id){
 tab3server <- function(id, data) {
   moduleServer(id, function(input, output, session) {
     
-    # Defining the column and the column values by which to filter
-    selected_column <- reactiveVal(NULL)
-    selected_values <- reactiveVal(NULL)
-    
     # Saving the data uploaded in another tab and passes to this module.
     dataset <- reactiveVal(NULL)
     observeEvent(data(), {
       dataset(data())
     })
+    
+    # Defining the column and the column values by which to filter
+    selected_column <- reactiveVal(NULL)
+    selected_values <- reactiveVal(NULL)
+    
+    # Storing the buttons for stratification layersn, names of layers, and
+    # number of layers
+    strat_layer_buttons <- reactiveVal(NULL)
+    strat_layer_names <- reactiveVal(NULL)
+    strat_layer_counter <- reactiveVal(0)
                  
     # When the filter button is clicked, show a dropwdown menu with which the 
     # user can select by which column to filter.
@@ -66,6 +77,7 @@ tab3server <- function(id, data) {
         output$value_dropdown <- NULL
       }
     })
+    
     # Displays the filtered data
     filtered_data <- reactive({
       req(input$value_select)
@@ -88,6 +100,57 @@ tab3server <- function(id, data) {
     output$filtered_table <- renderTable({
       filtered_data()
     })
+    
+    # Adding a new stratification layer
+    observeEvent(input$add_strat_layer_button, {
+      counter <- strat_layer_counter() + 1
+      strat_layer_counter(counter)
+      
+      # Creating a new button for new strata
+      ns <- session$ns
+      new_layer_button <- actionButton(ns(paste0("strat_layer_button_", counter)), 
+                                       paste("Schicht", counter))
+      
+      # Add the new strat layer button to the list of layer buttons
+      strat_buttons_list <- c(isolate(strat_layer_buttons()), list(new_layer_button))
+      strat_layer_buttons(strat_buttons_list)
+      
+      output$strat_layer_buttons_ui <- renderUI({
+        req(strat_layer_buttons())
+        strat_layer_buttons()
+      })
+    })
+    
+    # Outputting strat layer buttons UI
+    
+    
+    # Event handler for strat layer button click
+    observe({
+      # Checking for a strata button being pushed
+      layer_names_list <- lapply(seq_along(strat_layer_buttons()), function(i) {
+        ns <- session$ns
+        layer_button_id <- paste0("strat_layer_button_", i)
+        input_id <- paste0("layer_name_input_", i)
+        req(input[[layer_button_id]])
+        if (input[[layer_button_id]] > 0) {
+          # Allows for chosing a row base a stratification layer on
+          output$strata_rename_input_ui <- renderUI({
+            ns <- session$ns
+            selectInput(inputId = ns(input_id), "Schicht auswählen", 
+                        choices = colnames(dataset()),
+                        selected = selected_column())
+          })
+          observe({
+            updateActionButton(session, layer_button_id, label=input[[input_id]])
+          })
+        } else {
+          NULL
+        }
+      })
+      
+      strat_layer_names(unlist(strat_layer_names))
+    })
+    
   })
   
 }
