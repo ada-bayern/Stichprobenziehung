b609b33cc31077463320745276c23d9d17dbab4c
Aida Kreider <aida.kreider99@gmail.com>
2024-03-12 12:58:04 +0100

    included data transformation, from original to akten and summary + plot

--- modules/tab2_module.R
+++ modules/tab2_module.R
@@ -7,6 +7,8 @@ tab2ui <- function(id){
     br(),
     actionButton(ns("show_text"), "Daten einsehen"),
     tableOutput(ns("head")),
+    tableOutput(ns("head.akten")),
+    verbatimTextOutput(ns("summary1")),
     
     plotOutput(ns("plot1"))
     
@@ -18,25 +20,53 @@ tab2server <- function(id) {
     # Reactive value to store the uploaded dataframe
     uploaded_data <- reactiveVal(NULL)
     
+    my_akten <- reactiveVal(NULL)
+    
     # Upload file
     observeEvent(input$file, {
       uploaded_data(read.csv(input$file$datapath))
-    })
-    
-    # Output file information
-    output$file_info <- renderPrint({
-      req(input$file)
-      paste("Uploaded file:", input$file$name)
+      
+      akten <- group_by(uploaded_data(), 
+                        `Gericht`,
+                        `Aktenzeichen`,
+                        `Streitwert.in.EURO`,
+                        `Gesamtstreitgegenstand`,
+                        `Erledigungsgrund`,
+                        `Dauer.des.Verfahrens.in.Tagen`,
+                        `Archivstatus`,
+                        `Anbietungsgrund..manuell.erfasst.`,
+                        `Anbietungsgrund`)
+      akten <- summarise(akten, `Anzahl Beteiligte` = n()) 
+      akten <- as.data.frame(akten)
+      akten <- mutate(akten,Index = row_number())
+      
+      my_akten(akten)
+                        
     })
     
     observe({
+      #show head
       req(input$show_text)
       output$head <- renderTable({
         head(uploaded_data())
       })
-      output$plot1 <- renderPlot({
-        plot(uploaded_data()[,5])
+      
+      req(input$show_text)
+      output$head.akten <- renderTable({
+        head(my_akten())
       })
+      
+      output$summary1 <- renderPrint({
+        summary(my_akten())
+      })
+      
+      #plot 
+      output$plot1 <- renderPlot({
+        ggplot(my_akten(), aes(Gericht)) + 
+          geom_bar() + 
+          scale_x_discrete(guide = guide_axis(angle = 90))
+      })
+      
     })
     
     # Return uploaded data
